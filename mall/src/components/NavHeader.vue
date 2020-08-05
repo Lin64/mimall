88ad3f0c8254d5ef1fd4de67398aa34fcfbc22c1
Linc <2116759177@qq.com>
2020-08-05 17:43:00 +0800

    创建项目目录结构

--- mall/src/components/NavHeader.vue
+++ mall/src/components/NavHeader.vue
@@ -1,12 +1,11 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  
+    name: "nav-header",
+    
 }
 </script>
 
--- mall/src/pages/index.vue
+++ mall/src/pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+    <div>
+
+    </div>
 </template>
 
 <script>
 export default {
-  
+    name: "index"
 }
 </script>
 
